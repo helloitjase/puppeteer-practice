3155ad7261e3fc872cea28619a62ce93f39df580
Jason Chen <jason.sl.chen@gmail.com>
2020-02-11 10:50:58 -0800

    tried scrape to pdf

--- scrape.js
+++ scrape.js
@@ -1,26 +1,35 @@
 const puppeteer = require('puppeteer');
 
 let scrape = async() => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://books.toscrape.com/');
-  await page.waitFor(1000);
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://books.toscrape.com/');
+    // await page.waitFor(1000);
+    
+    // const result = await page.evaluate(() => {
+    //   let results = [];
+    //   let books = document.querySelectorAll('article');
+    //   for (let i = 0; i < books.length; i++) {
+    //     let title = books[i].querySelector('h3 a').innerText;
+    //     let price = books[i].querySelector('.price_color').innerText;
+    //     results.push({title, price});
+    //   }
+    //   return results;
+    // })
   
-  const result = await page.evaluate(() => {
-    let results = [];
-    let books = document.querySelectorAll('article');
-    for (let i = 0; i < books.length; i++) {
-      let title = books[i].querySelector('h3 a').innerText;
-      let price = books[i].querySelector('.price_color').innerText;
-      results.push({title, price});
-    }
-    return results;
-  })
-  browser.close();
-  return result;
+    await page.pdf({path: 'books.toScrape.pdf' });
+    browser.close();
+
+  } catch(error) {
+    console.log
+  }
+  // return result;
 
 }
 
-scrape().then((value) => {
-  console.log(value);
-})
+// scrape().then((value) => {
+//   console.log(value);
+// })
+
+scrape();
